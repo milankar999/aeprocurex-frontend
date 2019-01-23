ffcdea16d300ea24516a97babfc8226f09846647
sarika <sarika14@gmail.com>
2019-01-23 10:48:24 +0530

    add to code reciver,reqestor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,24 @@ import { CrmHeaderComponent } from './components/crm/crm-header/crm-header.compo
 import { PoTypeEntryComponent } from './components/crm/crm-po/po-entry/po-type-entry/po-type-entry.component';
 import { PoEntryCustomerSelectionComponent } from './components/crm/crm-po/po-entry/po-entry-customer-selection/po-entry-customer-selection.component';
 import { PoEntryProductComponent } from './components/crm/crm-po/po-entry/po-entry-product/po-entry-product.component';
+import { CrmHomeComponent } from './components/crm/crm-home/crm-home.component';
+import { PoEntryReceiverSelectionComponent } from './components/crm/crm-po/po-entry/po-entry-receiver-selection/po-entry-receiver-selection.component';
+import { PoEntryRequesterSelectionComponent } from './components/crm/crm-po/po-entry/po-entry-requester-selection/po-entry-requester-selection.component';
+import { PoEntrySupportingInfoComponent } from './components/crm/crm-po/po-entry/po-entry-supporting-info/po-entry-supporting-info.component';
+import { CrmFooterComponent } from './components/crm/crm-footer/crm-footer.component';
 
 const routes:Routes=[
   {path:'login',component:LoginComponent},
   {path:'crm/crmheader',component:CrmHeaderComponent},
   {path:'crm/crmheader/crmpo-type',component: PoTypeEntryComponent},
   {path:'crm/po-entry/po-entry-customer-selection', component:PoEntryCustomerSelectionComponent},
-  {path:'crm/po-entry/po-entry-product', component:PoEntryProductComponent}
+  {path:'crm/po-entry/po-entry-product', component:PoEntryProductComponent},
+  { path:'crm/po-entry/po-entry-receiver-selection',component: PoEntryReceiverSelectionComponent},
+  {path:'crm/crm-home',component:CrmHomeComponent},
+  {path:'crm/po-entry/po-entry-request-selection',component: PoEntryRequesterSelectionComponent},
+  {path:'crm/po-entry/po-entry-supportinginfo',component: PoEntrySupportingInfoComponent},
+  {path:'crm/crmfooter',component:CrmFooterComponent},
+
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CrmComponent } from './components/crm/crm.component';
